28053999daec1d15f1725d22aa3f435d3fce4d87
Aryan <a94h.73@gmail.com>
2022-09-15 17:47:36 -0400

    Add Login

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import json
+import hashlib
+import jwt
+import datetime
 
 app = Flask(__name__)
 
 # Configuring Database
 with open("db_config.json") as file:
     conf = json.load(file)
-    print(conf)
 
 username = conf["username"]
 password = conf["password"]
 db_name = conf["db_name"]
 
 app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{username}:{password}@localhost:5432/{db_name}"
+
 db = SQLAlchemy(app)
 
 
@@ -23,20 +26,44 @@ class User(db.Model):
     country = db.Column(db.String, nullable=False)
     city = db.Column(db.String, nullable=False)
 
-
+# Signup
 @app.route("/user", methods=['POST'])
 def add_user():
     username = request.form["username"]
-    password = request.form["password"]
+    password = hashlib.sha256(request.form["password"].encode()).hexdigest()
     country = request.form["country"]
     city = request.form["city"]
     new_user = User(username=username, password=password, country=country, city=city)
     db.session.add(new_user)
-    db.session.commit()
-    return jsonify(success=True, status_code=200)
+    try:
+        db.session.commit()
+        return jsonify(success=True, status_code=200, message="Successfully Signed Up!")
+    except Exception as e:
+        if e.orig.pgcode == "23505":
+            return jsonify(success=False, status_code=400, message="Username already exists!")
+        else:
+            return jsonify(success=False, status_code=400, message="An Error Happened!")
 
 
-if __name__=="__main__":
+# Login
+@app.route("/login", methods=['POST'])
+def login():
+    username = request.form["username"]
+    password = hashlib.sha256(request.form["password"].encode()).hexdigest()
+    user = User.query.filter_by(username=username, password=password).first()
+    if user is None:
+        return jsonify(success=False, status_code=400, message="User doesn't exist! Try Again!")
+    else:
+        payload = {
+            "username": username,
+            "exp": datetime.datetime.now(tz=datetime.timezone.utc) + datetime.timedelta(minutes=20)
+        }
+        key = "weath"
+        token = jwt.encode(payload, key)
+        return jsonify(success=True, status_code=200, message="Successfully Logged In!", jwt_token=token)
+
+
+if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     app.run(debug=True)
