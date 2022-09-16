71a2f2d17759d908437c8b09c9568468de51e99b
Aryan <a94h.73@gmail.com>
2022-09-15 20:12:47 -0400

    Add Authorization Decorator

--- app.py
+++ app.py
@@ -20,21 +20,33 @@ app.config["SQLALCHEMY_DATABASE_URI"] = f"postgresql://{username}:{password}@loc
 
 db = SQLAlchemy(app)
 
-def authorize(request):
-    username = request.args.get("username")
-    token = request.headers.environ.get("HTTP_AUTHORIZATION")
-    if username is None or token is None:
-        raise exceptions.BadRequest
 
-    token = token.split()[1]
-    token_decoded = jwt.decode(token, "weath", algorithms=["HS256"])
-    if username != token_decoded["username"]:
-        raise jwt.exceptions.InvalidTokenError
+def authorize(f):
+    def decorator():
+        try:
+            username = request.args.get("username")
+            token = request.headers.environ.get("HTTP_AUTHORIZATION")
+            if username is None or token is None:
+                raise exceptions.BadRequest
 
-    user = User.query.filter_by(username=username).first()
-    if user is None or token_decoded["password"] != user.password:
-        raise jwt.exceptions.InvalidTokenError
-    return user
+            token = token.split()[1]
+            token_decoded = jwt.decode(token, "weath", algorithms=["HS256"])
+            if username != token_decoded["username"]:
+                raise jwt.exceptions.InvalidTokenError
+
+            user = User.query.filter_by(username=username).first()
+            if user is None or token_decoded["password"] != user.password:
+                raise jwt.exceptions.InvalidTokenError
+
+            return f(user)
+        except jwt.exceptions.ExpiredSignatureError:
+            return jsonify(success=False, status_code=401, message="Token Expired! Please Login Again!")
+        except jwt.exceptions.InvalidTokenError:
+            return jsonify(success=False, status_code=401, message="Token Invalid!")
+        except exceptions.BadRequest:
+            return jsonify(success=False, status_code=400, message="Bad Request! Check The Request Data!")
+
+    return decorator
 
 class User(db.Model):
     username = db.Column(db.String, primary_key=True)
@@ -82,23 +94,18 @@ def login():
 
 # Update Location
 @app.route("/user", methods=['PUT'])
-def update_location():
-    try:
-        user = authorize(request)
-        new_city = request.form.get("city")
-        new_country = request.form.get("country")
-        if new_country is not None:
-            user.country = new_country
-        if new_city is not None:
-            user.city = new_city
-        db.session.commit()
-        return jsonify(success=False, status_code=200, message="Update Was Successful!")
-    except jwt.exceptions.ExpiredSignatureError:
-        return jsonify(success=False, status_code=401, message="Token Expired! Please Login Again!")
-    except jwt.exceptions.InvalidTokenError:
-        return jsonify(success=False, status_code=401, message="Token Invalid!")
-    except exceptions.BadRequest:
-        return jsonify(success=False, status_code=400, message="Bad Request! Check The Request Data!")
+@authorize
+def update_location(user):
+    new_city = request.form.get("city")
+    new_country = request.form.get("country")
+    if new_country is not None:
+        user.country = new_country
+    if new_city is not None:
+        user.city = new_city
+    db.session.commit()
+    return jsonify(success=True, status_code=200, message="Update Was Successful!")
+
+# Delete User
 
 
 if __name__ == "__main__":
